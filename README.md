99bc800bc9f1fd9faaf63e273585a22cd8491b8b
scott.lan <scott.lan@taisys.com>
2016-11-17 09:42:25 +0800

    update config port

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ node sample01.js
 
 - By WebSocket Push Image Buffer to Client Browser
 - Ref: https://www.npmjs.com/package/livecam
+- Ref: https://www.npmjs.com/package/websockets-streaming-audio
 - Search Keyword: live streaming nodejs cam webcam
--- sample01.js
+++ sample01.js
@@ -2,11 +2,47 @@
 
 const LiveCam = require('livecam');
 const webcam_server = new LiveCam({
-    "start" : function() {
+    // address and port of the webcam UI
+    'ui_addr' : '0.0.0.0',
+    'ui_port' : 11000,
+
+    // address and port of the webcam Socket.IO server
+    // this server broadcasts GStreamer's video frames
+    // for consumption in browser side.
+    'broadcast_addr' : '0.0.0.0',
+    'broadcast_port' : 12000,
+
+    // address and port of GStreamer's tcp sink
+    'gst_tcp_addr' : '0.0.0.0',
+    'gst_tcp_port' : 10000,
+
+    // callback function called when server starts
+    'start' : function() {
         console.log('WebCam server started!');
     },
-    "ui_addr" : "0.0.0.0",
-    "ui_port" : 11000,
+
+    // webcam object holds configuration of webcam frames
+    'webcam' : {
+
+        // should frames be converted to grayscale (default : false)
+        'grayscale' : false,
+
+        // should width of the frame be resized (default : 0)
+        // provide 0 to match webcam input
+        'width' : 360,
+
+        // should height of the frame be resized (default : 0)
+        // provide 0 to match webcam input
+        'height' : 240,
+
+        // should a fake source be used instead of an actual webcam
+        // suitable for debugging and development (default : false)
+        'fake' : false,
+
+        // framerate of the feed (default : 0)
+        // provide 0 to match webcam input
+        'framerate' : 25
+    }
 });
  
 webcam_server.broadcast();
